6b9064fc4b17055fd83cc96d961c5402fa28e5f4
unknown <you@example.com>
2024-04-24 13:05:15 -0300

    criação inicial do serviço de cadastro

---
+++ src/main/java/Services/PessoaService.java
@@ -0,0 +1,71 @@
+package Services;
+
+import entidades.*;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Scanner;
+
+public class PessoaService {
+    Scanner scanner = new Scanner(System.in);
+
+    public Medico criarNovoMedico(){
+
+        LocalDate dataDeNascimentoMedico1 = LocalDate.of(1988, 2, 1);
+
+        ArrayList<Endereco> enderecoMedico1 = new ArrayList<>();
+        Endereco endereco1 = new Endereco("MG", "Juiz de Fora", "Bairro de Lourdes", "Rua A", 326);
+        enderecoMedico1.add(endereco1);
+
+        ArrayList<String> certificacoes = new ArrayList<>();
+        String certificoesMedico1 = new String("diploma de medicina ds UFJF, diploma de oncologia na residencia da UFRJ");
+        certificacoes.add(certificoesMedico1);
+
+        Medico medico1 = new Medico("Jose Aldo", 36, 'H', 181-123-567-13, "MG-19389", dataDeNascimentoMedico1, enderecoMedico1, 1.75, 80.5, "brasileiro", "Solteiro", "Negro", "Médico",3298845-1328, 3235-8819, "Oncologista", 40.000, certificacoes);
+
+
+        return medico1;
+    }
+    public Paciente criarNovoPaciente(){
+
+        LocalDate dataDeNascimentoPaciente1 = LocalDate.of(1988, 2, 1);
+
+        ArrayList<Endereco> enderecoPaciente1 = new ArrayList<>();
+        Endereco endereco1 = new Endereco("MG", "Juiz de Fora", "Bairro São Mateus", "Rua Claudio Honorio", 36);
+        enderecoPaciente1.add(endereco1);
+
+        Paciente paciente1 = new Paciente("Reginaldo Manzoti", 19, 'H', 198-245-886-14, "MG-936712", dataDeNascimentoPaciente1, enderecoPaciente1, 1.89, 75.0, "Brasileiro", "Casado", "Branco", "Jogador de basquete", 3298813-2928, 3235-9977, "reginaldoman@gmail.com", "Plano Unimed");
+
+        return paciente1;
+    }
+    public Atendente criarNovoAtendente(Medico medico1){
+
+        Medico medico = criarNovoMedico();
+        Atendente atendente1 = criarNovoAtendente(medico1);
+
+
+        LocalDate dataDeNascimentoAtendente1 = LocalDate.of(1988, 2, 1);
+
+        ArrayList<Endereco> enderecoAtendente1 = new ArrayList<>();
+        Endereco endereco1 = new Endereco("MG", "Belo Horizonte", "Bairro Joseph Alves", "Rua Manoel de castanha", 901);
+        enderecoAtendente1.add(endereco1);
+
+
+        ArrayList<Consulta> consultas = new ArrayList<>();
+        Atendente atendente1 = new Atendente("Aline Queiroz", 29, 'M', 198-295-838-80, "RJ-12423", dataDeNascimentoAtendente1, enderecoAtendente1, 1.60, 55.0, "Brasileira", "Solteira", "pardo", "Atendente de clinica médica", 3298856-7789, 3232-4578,"" );
+        LocalDateTime diaDaConsulta1 = LocalDateTime.of(2024, 5, 13, 13, 0);
+        Consulta consulta1 = new Consulta(atendente1, diaDaConsulta1, medico1,  );
+
+        return atendente1;
+    }
+
+
+
+
+
+
+
+}
