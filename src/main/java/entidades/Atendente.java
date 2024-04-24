1d15208b24c2d2a2ad96cf7561fac4643b647822
unknown <you@example.com>
2024-04-24 13:12:51 -0300

    alterando a função agenda de atendente para não ocorrer o erro de nescessidade de agenda para a formação de um atendente novo

--- src/main/java/entidades/Atendente.java
+++ src/main/java/entidades/Atendente.java
@@ -4,13 +4,12 @@ import java.time.LocalDate;
 import java.util.ArrayList;
 
 public class Atendente extends Pessoa {
-    private ArrayList<Consulta> agenda;
+
     private String cargo;
     private double salario;
 
-    public Atendente(String nome, int idade, char genero, int cpf, String rg, LocalDate dataDeNascimento, ArrayList<Endereco> listaDeEnderecos, double altura, double peso, String nacionalidade, String estadoCivil, String etnia, String profissao, long numeroCelular, long numeroTelefoneFixo, ArrayList<Consulta> agenda, String cargo, double salario) {
+    public Atendente(String nome, int idade, char genero, int cpf, String rg, LocalDate dataDeNascimento, ArrayList<Endereco> listaDeEnderecos, double altura, double peso, String nacionalidade, String estadoCivil, String etnia, String profissao, long numeroCelular, long numeroTelefoneFixo, String cargo, double salario) {
         super(nome, idade, genero, cpf, rg, dataDeNascimento, listaDeEnderecos, altura, peso, nacionalidade, estadoCivil, etnia, profissao, numeroCelular, numeroTelefoneFixo);
-        this.agenda = agenda;
         this.cargo = cargo;
         this.salario = salario;
     }
@@ -18,18 +17,9 @@ public class Atendente extends Pessoa {
     @Override
     public String toString() {
         return "Atendente{" +
-                "agenda=" + agenda +
                 ", cargo='" + cargo + '\'' +
                 ", salario=" + salario +
                 '}';
-    }
-
-    public ArrayList<Consulta> getAgenda() {
-        return agenda;
-    }
-
-    public void setAgenda(ArrayList<Consulta> agenda) {
-        this.agenda = agenda;
     }
 
     public String getCargo() {
